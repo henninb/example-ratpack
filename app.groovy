88299449c62181c53049224ab08aa0bebbf7e18e
Brian Henning <henninb@msn.com>
2020-12-06 11:03:51 -0600

    updates

--- app.groovy
+++ app.groovy
@@ -5,7 +5,7 @@
 @GrabResolver(name='restlet', root='http://maven.restlet.org/')
 // @GrabResolver(name='restlet', root='http://maven.restlet.org/')
 @Grab('io.ratpack:ratpack-groovy:1.8.0')
-@Grab('org.codehaus.groovy:groovy-all:2.5.14')
+// @Grab('org.codehaus.groovy:groovy-all:2.5.14')
 //@Grab('org.codehaus.groovy:groovy-all:2.5.4')
 //@Grab('io.ratpack:ratpack-groovy:1.3.3')
 // @Grab('org.slf4j:slf4j-simple:1.7.0')
