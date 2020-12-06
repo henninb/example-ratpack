43e9126fe0cfc7aab9dfbbadc2c48784204420f3
Brian Henning <henninb@msn.com>
2020-12-06 08:14:19 -0600

    updates

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # example ratpack
 
+sdk install groovy 2.5.4
 grape -V -Dhttps.protocols=TLSv1.2 install io.ratpack ratpack-groovy 1.3.3
--- app.groovy
+++ app.groovy
@@ -2,6 +2,8 @@
 //@Grab('io.ratpack:ratpack-groovy:1.3.3')
 // @Grab('org.slf4j:slf4j-simple:1.7.0')
 
+//groovysh { groovyVersion = '2.3.0' }
+
 import static ratpack.groovy.Groovy.ratpack
 
 println('test')
