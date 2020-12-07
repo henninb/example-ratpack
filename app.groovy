05dc172cdeaee557c6daa9972a7ca34af86b853c
Brian Henning <henninb@msn.com>
2020-12-06 19:34:53 -0600

    updates

--- app.groovy
+++ app.groovy
@@ -1,14 +1,7 @@
 #! /usr/bin/env groovy
 
-//@GrabConfig(autoDownload=true)
-//@GrabResolver(name='restlet', root='http://maven.restlet.org/')
-//@GrabResolver(name='restlet', root='http://maven.restlet.org/')
-// @GrabResolver(name='restlet', root='http://maven.restlet.org/')
 @Grab('io.ratpack:ratpack-groovy:1.8.0')
 @Grab('org.slf4j:slf4j-simple:1.7.0')
-
-
-//groovysh { groovyVersion = '2.3.0' }
 
 import static ratpack.groovy.Groovy.ratpack
 
@@ -20,6 +13,7 @@ String test = 'test1|test2|test3|test4'
 String[] list = test.split('\\|')
 list.each {it -> println it}
 List<String> myList = Arrays.asList(list)
+def map = [:]
 
 ratpack {
   handlers {
