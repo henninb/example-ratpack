8ce641471bbf34905d5756b5dbfd07d0a94e4fec
Brian Henning <henninb@msn.com>
2020-12-15 07:47:20 -0600

    updates

--- app.groovy
+++ app.groovy
@@ -1,18 +1,15 @@
 #! /usr/bin/env groovy
 
+@GrabConfig(systemClassLoader=true)
 @Grab('io.ratpack:ratpack-groovy:1.8.0')
 @Grab('org.slf4j:slf4j-simple:1.7.0')
 @Grab('org.postgresql:postgresql:42.2.18')
 @Grab('org.xerial:sqlite-jdbc:3.34.0')
+//@Grab('ch.qos.logback:logback-classic:1.2.3')
 
 import static ratpack.groovy.Groovy.ratpack
 import java.sql.*
-//import org.sqlite.SQLite
 import groovy.sql.Sql
-
-Class.forName("org.sqlite.JDBC")
-
-def sql = Sql.newInstance("jdbc:sqlite:pluto.db", "org.sqlite.JDBC")
 
 void exampleFunction() {
   println('test')
@@ -23,6 +20,11 @@ void exampleFunction() {
   list.each {it -> println it}
   List<String> myList = Arrays.asList(list)
   def map = [:]
+
+  def sql = Sql.newInstance("jdbc:sqlite:pluto.db", "org.sqlite.JDBC")
+  sql.eachRow("select * from t_channel where channel=50") {
+    println("description=${it.description}, category=${it.category}")
+  }
 }
 
 //curl 'http://localhost:5050'
