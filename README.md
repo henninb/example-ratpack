a0649a75c41083fae84c3f2cb613216166748ccc
Brian Henning <henninb@msn.com>
2020-12-15 07:10:11 -0600

    added plutotv

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ grape -V -Dhttps.protocols=TLSv1.2 install io.ratpack ratpack-groovy 1.3.3
 
 
 lazybones create ratpack 1.6.0 my-rat-app
+
+sqllite3 pluto.db
+
+create table t_pluto(channel text, description text, category text);
+create table t_pluto(id smallint primary key, channel text, description text, category text);
+create table t_pluto(id INTEGER PRIMARY KEY AUTOINCREMENT, channel text, description text, category text);
--- app.groovy
+++ app.groovy
@@ -2,22 +2,34 @@
 
 @Grab('io.ratpack:ratpack-groovy:1.8.0')
 @Grab('org.slf4j:slf4j-simple:1.7.0')
+@Grab('org.postgresql:postgresql:42.2.18')
+@Grab('org.xerial:sqlite-jdbc:3.34.0')
 
 import static ratpack.groovy.Groovy.ratpack
+import java.sql.*
+//import org.sqlite.SQLite
+import groovy.sql.Sql
 
-println('test')
+Class.forName("org.sqlite.JDBC")
 
-def x = [1,2,3,4,5]
-println x.first()
-String test = 'test1|test2|test3|test4'
-String[] list = test.split('\\|')
-list.each {it -> println it}
-List<String> myList = Arrays.asList(list)
-def map = [:]
+def sql = Sql.newInstance("jdbc:sqlite:pluto.db", "org.sqlite.JDBC")
 
+void exampleFunction() {
+  println('test')
+  def x = [1,2,3,4,5]
+  println x.first()
+  String test = 'test1|test2|test3|test4'
+  String[] list = test.split('\\|')
+  list.each {it -> println it}
+  List<String> myList = Arrays.asList(list)
+  def map = [:]
+}
+
+//curl 'http://localhost:5050'
 ratpack {
   handlers {
     get {
+       exampleFunction()
        render "hello world"
     }
   }
